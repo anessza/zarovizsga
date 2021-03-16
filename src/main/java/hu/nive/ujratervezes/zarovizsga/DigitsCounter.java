13b580148a664726ffc9f2db1b940f186af8d203
anessza <42357124+anessza@users.noreply.github.com>
2021-03-16 18:50:48 +0100

    Számjegyek (DigitsCounter)

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/DigitsCounter.java
@@ -0,0 +1,12 @@
+package hu.nive.ujratervezes.zarovizsga;
+/*
+A DigitsCounter osztálynak legyen egy int getCountOfDigits(String s) metódusa,
+mely megszámolja, hogy hány különböző számjegy van a bemeneti Stringben.
+ */
+
+public class DigitsCounter {
+
+    public int getCountOfDigits(String s) {
+
+    }
+}
